d7abc541fd4b3e65f6a799172cb2bb8b69a73fe9
Agustín Palombo <agpalombo99@gmail.com>
2023-08-11 20:46:11 -0300

    Desafio-3
    Cambie la linea port por simplemente  3000

--- ProductManager/src/index.js
+++ ProductManager/src/index.js
@@ -50,7 +50,7 @@ app.delete('/productos/:id', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor Express en ejecución en el puerto ${PORT}`);
 });
