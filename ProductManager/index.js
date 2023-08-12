5cdf01c64abf275e622d4a87155de64d65315b5c
Agustín Palombo <agpalombo99@gmail.com>
2023-08-12 12:01:59 -0300

    Desafio-3 Express server
    No funciona de manera correcta el metodos GET en postman ya que la prueba devuelve los datos de manera desordenada

--- ProductManager/index.js
+++ ProductManager/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ProductManager = require('./ProductManager');
+const ProductManager = require('./src/ProductManager');
 
 const app = express();
 app.use(express.json());
